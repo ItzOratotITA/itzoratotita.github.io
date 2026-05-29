fb62fbe4c52331ae31918f95d8cde7254d7dd63d
oratot <152989182+ItzOratotITA@users.noreply.github.com>
2026-05-29 22:07:24 +0200

    backgrounds from school trip :)

---
+++ background.js
@@ -0,0 +1,11 @@
+const backgrounds = [
+  "/assets/louvre.avif",
+  "/assets/bridge_view.avif",
+  "/assets/castle_view.avif",
+  "/assets/mar_grande_view.avif",
+];
+
+const randomBackground =
+  backgrounds[Math.floor(Math.random() * backgrounds.length)];
+
+document.body.style.setProperty("--random-bg", `url("${randomBackground}")`);
